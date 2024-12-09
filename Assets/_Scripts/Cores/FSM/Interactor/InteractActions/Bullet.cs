0a06e6b0e40661b8b811bc302cf8630714d81b0b
Caesar_home <2323182223@qq.com>
2024-12-09 23:09:00 +0800

    金克斯射击

--- Assets/_Scripts/Cores/FSM/Interactor/InteractActions/Bullet.cs
+++ Assets/_Scripts/Cores/FSM/Interactor/InteractActions/Bullet.cs
@@ -10,75 +10,99 @@ public struct SBulletState
 
 namespace FSM
 {
-    public class Bullet : MonoBehaviour,ITimeBackable
+    public class Bullet : MonoBehaviour
     {
-        float velocity = 10f;
+        float _velocity = 10f;
+        float _damage;
+        AttackUid _attackUid;
         bool _isShoot=false;
         float lifeTime;
         Vector3 _dir;
+        Rigidbody rb;
+
+        PlayerMovement playerMovement;
+        private void Awake()
+        {
+            _attackUid = new AttackUid();
+            rb = GetComponent<Rigidbody>();
+            playerMovement=DataModule.Resolve<GamePlayDM>().GetPlayer().Movement;
+        }
 
         #region TimeBack
         bool _isBack;
-        TimeBackManager _backManager;
-        Dictionary<int, SBulletState> _timeRecords = new(100);
+        //TimeBackManager _backManager;
+        //Dictionary<int, SBulletState> _timeRecords = new(100);
 
-        private void OnEnable()
-        {
-            _backManager = DataModule.Resolve<GamePlayDM>().TimeBackManager;
-            _backManager.Register(this);
-        }
-        private void OnDisable()
-        {
-            _backManager.UnRegister(this);
-        }
-        public void TimeStateRecord(int index)
-        {
-            if ( _timeRecords.ContainsKey(index) )
-                _timeRecords[index] = new SBulletState()
-                {
-                    pos = transform.position,
-                    isActive = gameObject.activeSelf,
-                };
-            else
-                _timeRecords.Add(index, new SBulletState()
-                {
-                    pos = transform.position,
-                    isActive = gameObject.activeSelf,
-                });
-        }
-        public void TimeBackStart(int index)
-        {
-            _isBack = true;
-        }
-        public void TimeBackTick(int index)
-        {
-            if ( _timeRecords.ContainsKey(index) )
-            {
-                transform.position = _timeRecords[index].pos;
-                gameObject.SetActive(_timeRecords[index].isActive);
-                _timeRecords.Remove(index);
-            }
-        }
-        public void TimeBackEnd(int index)
-        {
-            _isBack = false;
-        }
+        //private void OnEnable()
+        //{
+        //    _backManager = DataModule.Resolve<GamePlayDM>().TimeBackManager;
+        //    _backManager.Register(this);
+        //}
+        //private void OnDisable()
+        //{
+        //    _backManager.UnRegister(this);
+        //}
+        //public void TimeStateRecord(int index)
+        //{
+        //    if ( _timeRecords.ContainsKey(index) )
+        //        _timeRecords[index] = new SBulletState()
+        //        {
+        //            pos = transform.position,
+        //            isActive = gameObject.activeSelf,
+        //        };
+        //    else
+        //        _timeRecords.Add(index, new SBulletState()
+        //        {
+        //            pos = transform.position,
+        //            isActive = gameObject.activeSelf,
+        //        });
+        //}
+        //public void TimeBackStart(int index)
+        //{
+        //    _isBack = true;
+        //}
+        //public void TimeBackTick(int index)
+        //{
+        //    if ( _timeRecords.ContainsKey(index) )
+        //    {
+        //        transform.position = _timeRecords[index].pos;
+        //        gameObject.SetActive(_timeRecords[index].isActive);
+        //        _timeRecords.Remove(index);
+        //    }
+        //}
+        //public void TimeBackEnd(int index)
+        //{
+        //    _isBack = false;
+        //}
 
         #endregion
-        public void Shoot(Vector3 dir)
+        public void Fire(Vector3 dir,float damage,float velocity)
         {
+            _damage= damage;
+            _velocity = velocity;
+            _attackUid.Reset();
             _isShoot = true;
             _dir = dir;
-            lifeTime = _backManager.GameTime;
+            //lifeTime = _backManager.GameTime;
         }
 
         private void Update()
         {
-            if (_isShoot)
+            if (_isShoot&&!_isBack)
             {
-                transform.position += _dir * velocity;
-                if(lifeTime+3f<_backManager.GameTime)
+                rb.linearVelocity = _dir * (_velocity+playerMovement.MapMoveSpeed);
+                if(lifeTime+3f>Time.time)
                     Destroy(gameObject);
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            var player =other.GetComponentInParent<PlayerEntity>();
+            if(player!=null)
+            {
+                player.Combat.ReceiveDamage(_attackUid.value,_damage);
+                gameObject.SetActive(false);
             }
         }
     }
--- Assets/_Scripts/Cores/FSM/Interactor/InteractActions/InterackFireAttackAction.cs
+++ Assets/_Scripts/Cores/FSM/Interactor/InteractActions/InterackFireAttackAction.cs
@@ -8,15 +8,12 @@ namespace FSM
     {
         Transform player;
 
-        [SerializeField]
-        TriggerSense sense;
-
         public Transform checkPos;
         public string animationName;
 
         public float damage=10f;
+        public float bulletVelocity=10f;
         public GameObject go_bullet;
-        AttackUid _attackUid=new();
 
         private void Start()
         {
@@ -27,34 +24,19 @@ namespace FSM
             return player.position.x > checkPos.position.x;
         }
 
+
         public override void DoAction()
         {
             base.DoAction();
-            sense.AddHandler(OnTrigger, PlayerCheck);
             entity.Animator.SetTrigger(animationName);
-            _attackUid.Reset();
             
         }
         public override void EndAction()
         {
             base.EndAction();
-            sense.RemoveHandler(OnTrigger, PlayerCheck);
+            var bullet= Instantiate(go_bullet, transform.position, Quaternion.FromToRotation(-transform.forward,transform.forward)).GetComponent <Bullet>();
+            bullet.Fire(transform.right, damage, bulletVelocity);
         }
 
-        bool PlayerCheck(Collider collider)
-        {
-            var player = collider.GetComponentInParent<PlayerEntity>();
-            return player != null;
-        }
-
-        private void OnTrigger(Collider other,bool isEnter)
-        {
-            if(isEnter)
-            {
-                var player = other.GetComponentInParent<PlayerEntity>();
-                if(player!=null)
-                    player.Combat.ReceiveDamage(_attackUid.value, damage);
-            }
-        }
     }
 }
